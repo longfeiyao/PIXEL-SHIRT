01c2a7cfcae2a2263c68d4a81af2cbd0060d376c
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2015-11-01 19:38:24 +0100

    remove unused import

--- src/com/pixel/entities/Panier.java
+++ src/com/pixel/entities/Panier.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 @Entity
